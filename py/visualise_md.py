a68250e0570434903e9fa591c98f332761cf8979
jesstytam <j.tam@unsw.edu.au>
2023-08-16 00:33:46 +0000

    pushing visualisation script again

--- py/visualise_md.py
+++ py/visualise_md.py
@@ -1,10 +1,27 @@
 # Render bounding boxes on our images
+import subprocess
+
 output_file_path = 'home/jess2/data/wild_deserts/processed/md_output.json'
 visualisation_dir = 'home/jess2/data/wild_deserts/processed/md_output'
+img_dir = 'home/jess2/data/wild_deserts/Beyond the Fence- Tagged'
 
-for dir in 'home/jess2/data/wild_deserts/Beyond the Fence- Tagged':
-	images_dir = dir
-	!python ~/git/CameraTraps/visualization/visualize_detector_output.py "$output_file_path" "$visualisation_dir" --confidence 0.2 --images_dir "$images_dir"
+for dir in img_dir:
+    images_dir = dir
+    command = [
+            "python",
+            "home/jess2/git/CameraTraps/visualization/visualize_detector_output.py",
+            output_file_path,
+            visualisation_dir,
+            "--confidence",
+            "0.2",
+            "images_dir",
+            images_dir
+            ]
+    subprocess.run(command, check=True)
+
+#for dir in 'home/jess2/data/wild_deserts/Beyond the Fence- Tagged':
+#	images_dir = dir
+#	subprocess.check_output(command "$output_file_path" "$visualisation_dir" --confidence 0.2 --images_dir "$images_dir")
 
 # Show the images with bounding boxes
 import os
