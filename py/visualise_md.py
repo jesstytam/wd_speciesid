15c4365411202437a4f3d7b915726f550668cc47
jesstytam <j.tam@unsw.edu.au>
2023-08-16 00:03:52 +0000

    running script to create images with bounding boxes

--- py/visualise_md.py
+++ py/visualise_md.py
@@ -1,12 +1,7 @@
 # Render bounding boxes on our images
-visualisation_dir = 'home/jess2/data/wild_deserts/Beyond the Fence- Tagged/PCAM01'
-#!python ~/git/CameraTraps/visualization/visualize_detector_output.py "$output_file_path" "$visualisation_dir" --confidence 0.2 --images_dir "$images_dir"
+output_file_path = 'home/jess2/data/wild_deserts/processed/md_output.json'
+visualisation_dir = 'home/jess2/data/wild_deserts/processed/md_output'
 
-# Show the images with bounding boxes
-import os
-from PIL import Image
-
-for vis_file_name in os.listdir(visualisation_dir):
-      print(vis_file_name)
-      im = Image.open(os.path.join(visualisation_dir, vis_file_name))
-      display(im)
+for dir in 'home/jess2/data/wild_deserts/Beyond the Fence- Tagged':
+	images_dir = dir
+	!python ~/git/CameraTraps/visualization/visualize_detector_output.py "$output_file_path" "$visualisation_dir" --confidence 0.2 --images_dir "$images_dir"
--- py/visualise_md.sh
+++
@@ -1,9 +0,0 @@
-# Render bounding boxes on our images
-output_file_path = '~/data/wild_deserts/processed/md_output.json'
-visualisation_dir = '~/data/wild_deserts/processed/md_output'
-
-for dir in '~/data/wild_deserts/Beyond the Fence- Tagged'
-do
-	images_dir = dir
-	!python /~/git/CameraTraps/visualisation/visualise_detector_output.py "$output_file_path" "$visualisation_dir" --confidence 0.2 --images_dir "$images_dir"
-done
