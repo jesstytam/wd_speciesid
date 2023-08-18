1c860128405fb1cd540cf0ce195934ebad38546b
jesstytam <j.tam@unsw.edu.au>
2023-08-18 05:25:53 +0000

    trying to decide how to split

--- scripts/coco_to_yolo.py
+++ scripts/coco_to_yolo.py
@@ -1,5 +1,5 @@
 from pylabel import importer
 
-path_to_annotations = 'wd_speciesid/data/intermediate/md_coco.json'
-outpath = 'wd_speciesid/data/intermediate/coco_yolo.json'
+path_to_annotations = 'wd_speciesid/data/intermediate/coco.json'
+outpath = 'wd_speciesid/data/intermediate/yolo.json'
 importer.ImportCoco(path_to_annotations).export.ExportToYoloV5(outpath)
